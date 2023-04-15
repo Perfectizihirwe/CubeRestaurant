f53eb65f876194440b133457690f5fd3db8dbeda
Perfectizihirwe <perfectizihirwe@gmail.com>
2023-04-15 10:35:29 +0200

    Defining app structure

---
+++ src/utils/dataStore.js
@@ -0,0 +1,26 @@
+export const menu = [
+  {
+    id: 1,
+    name: "Salad and Chips",
+    price: 5000,
+    image: "https://i.imgur.com/8kRmUQx.jpg",
+  },
+  {
+    id: 2,
+    name: "Salad and Chips",
+    price: 5000,
+    image: "https://i.imgur.com/8kRmUQx.jpg",
+  },
+  {
+    id: 3,
+    name: "Salad and Chips",
+    price: 5000,
+    image: "https://i.imgur.com/8kRmUQx.jpg",
+  },
+  {
+    id: 4,
+    name: "Salad and Chips",
+    price: 5000,
+    image: "https://i.imgur.com/8kRmUQx.jpg",
+  },
+];
